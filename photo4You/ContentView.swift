158fa0682567d448bb35fe3336b62217c124bc1b
Gerrit Grunwald <han.solo@mac.com>
2025-11-12 09:50:05 +0100

    Initial Commit

---
+++ photo4You/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  photo4You
+//
+//  Created by Gerrit Grunwald on 12.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ photo4You/photo4YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  photo4YouApp.swift
+//  photo4You
+//
+//  Created by Gerrit Grunwald on 12.11.25.
+//
+
+import SwiftUI
+
+@main
+struct photo4YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
